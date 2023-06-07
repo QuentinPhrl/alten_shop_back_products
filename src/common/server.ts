46185b413c6bec1472fc2c1509b4f7bbe2694d2c
QuentinP <puechguiral81@hotmail.fr>
2023-06-08 00:06:25 +0200

    Remove error middleware from server.ts

--- src/common/server.ts
+++ src/common/server.ts
@@ -12,11 +12,6 @@ export const Server = (port: number) => {
 
   app.use('/', router);
 
-  app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-    console.error(err.stack);
-    res.status(500).json({'Error message': err.message});
-  });
-
   app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
